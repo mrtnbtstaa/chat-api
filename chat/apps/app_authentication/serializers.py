c726c81c939b207dd9912c1ada1ad1ffeb3a4ac2
mrtnbtstaa <martinbautista0288@outlook.com>
2026-03-18 18:59:21 +0800

    feat: Authentication WIP

--- chat/apps/app_authentication/serializers.py
+++ chat/apps/app_authentication/serializers.py
@@ -2,14 +2,20 @@ from rest_framework_simplejwt.serializers import TokenRefreshSerializer, TokenOb
 from rest_framework import serializers
 from django.db import transaction
 from apps.core.utils.validators import is_field_empty
-from apps.core.utils.raise_validation import raise_validation
+from apps.core.utils.helpers import raise_validation
 from django.contrib.auth import login, authenticate
 from rest_framework_simplejwt.exceptions import TokenError
+from .models import User, Profile
 
 class CustomTokenRefreshSerializer(TokenRefreshSerializer):
 
     refresh = serializers.CharField(
-        blank="Refresh token is required"
+        allow_null=False,
+        error_messages={
+            "required": "Refresh token is required",
+            "blank": "Refresh token cannot be blank",
+            "invalid": "Refresh token is invalid"
+        }
     )
 
     def validate(self, attrs):
@@ -84,6 +90,59 @@ class CustomLoginObtainPairSerializer(TokenObtainPairSerializer):
 
         return token
 
+
+class RegisterSerializer(serializers.Serializer):
+
+    profile_image = serializers.ImageField(allow_null=True)
+
+    username = serializers.CharField(
+        allow_null=False,
+        error_messages={
+            "required": "Username is required.",
+            "blank": "Username cannot be blank.",
+            "invalid": "Username is invalid.",
+            "null": "Username cannot be null."
+        }
+    )
+
+    password = serializers.CharField(
+        allow_null=False,
+        min_length=3,
+        error_messages={
+            "required": "Password is required.",
+            "blank": "Password cannot be blank.",
+            "invalid": "Password is invalid.",
+            "null": "Password cannot be null.",
+            "min_length": "Password must be atleast 8 characters."
+        },
+    )
+
+    confirm_password = serializers.CharField(
+        allow_null=False,
+        min_length=3,
+        error_messages={
+            "required": "Confirm password is required.",
+            "blank": "Confirm password cannot be blank.",
+            "invalid": "Confirm password is invalid.",
+            "null": "Confirm password cannot be null.",
+            "min_length": "Confirm password must be atleast 8 characters."
+        }
+    )
+
+    def validate(self, attrs):
+
+
+        return super().validate(attrs)
+    
+
+    def create(self, validated_data):
+        
+        user = User.objects.create_user(
+            username=validated_data.get('username'),
+            password=validated_data.get('password')
+        )
+
+        
         
 
 
