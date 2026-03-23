c7128304ef35e433f80260823b7413ac99c8b48a
mrtnbtstaa <martinbautista0288@outlook.com>
2026-03-23 21:45:34 +0800

    feat: removed nickname from the auth model

--- chat/apps/app_authentication/models.py
+++ chat/apps/app_authentication/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class User(AbstractUser, UUIDTimestampModel):
 
     is_online = models.BooleanField(default=False)
-    nickname = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.username
